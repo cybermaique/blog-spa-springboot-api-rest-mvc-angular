ebeadac5249d8c79e2896587907a25cce9793e8b
cybermaique <maiqued.18@gmail.com>
2022-10-23 20:40:54 -0300

    feat(backend): model

---
+++ backend/src/main/java/com/example/todo/Model/Tarefa.java
@@ -0,0 +1,48 @@
+package com.example.todo.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_tarefa")
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="Id")
+    private Integer Id;
+    
+    @Column(name="Tarefa")
+    private String Tarefa;
+    
+    @Column(name="Concluida")
+    private Boolean Concluida;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public String getTarefa() {
+        return Tarefa;
+    }
+
+    public void setTarefa(String Tarefa) {
+        this.Tarefa = Tarefa;
+    }
+
+    public Boolean getConcluida() {
+        return Concluida;
+    }
+
+    public void setConcluida(Boolean Concluida) {
+        this.Concluida = Concluida;
+    }
+    
+}
