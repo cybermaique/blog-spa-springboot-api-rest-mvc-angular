46da745e7dd6e031d57fab2a22ec56b90d391d5b
cybermaique <maiqued.18@gmail.com>
2022-10-23 21:55:59 -0300

    feat(backend): service finish and initial created components

---
+++ frontend/src/app/SERVICE/tarefa.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TarefaService {
+  url = 'http://localhost:8080/api/tarefas';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  getTaskById(id: string): Observable<any> {
+    return this.http.get(this.url + '/' + id);
+  }
+
+  saveTask(tarefa: Tarefa): Observable<any> {
+    return this.http.post(this.url, tarefa);
+  }
+
+  editTask(id: string, tarefa: Tarefa): Observable<any> {
+    return this.http.put(this.url + '/' + id, tarefa);
+  }
+
+  deleteTask(id: string): Observable<any> {
+    return this.http.delete(this.url + '/' + id);
+  }
+}
+
+//db data
+export interface Tarefa {
+  id: string;
+  tarefa: string;
+  concluida: boolean;
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './COMPONENTS/start/start.component';
+import { EditComponent } from './COMPONENTS/edit/edit.component';
+import { AddComponent } from './COMPONENTS/add/add.component';
+import { NavbarComponent } from './COMPONENTS/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    EditComponent,
+    AddComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
